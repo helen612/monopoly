47c98ddfa8680d923dd9401db8a384f4866914ac
Helen Krigina <helen.krigina@gmail.com>
2023-01-19 09:43:25 +0300

    first commit

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject loadingScreen;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
